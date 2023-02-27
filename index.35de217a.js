ba23015836ba8087bd96bb842a6927613491ebcf
Code Sandbox <hello@codesandbox.io>
2023-02-27 04:34:16 +0000

    Deployment from Codesandbox.

---
+++ index.35de217a.js
@@ -0,0 +1,2 @@
+!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{};e.__DEV__=!1,e.__TEST__=!1}();
+//# sourceMappingURL=index.35de217a.js.map
---
+++ index.da6963f0.js
@@ -0,0 +1,2 @@
+var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{};e.__DEV__=!1,e.__TEST__=!1;
+//# sourceMappingURL=index.da6963f0.js.map
